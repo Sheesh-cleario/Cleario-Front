d905e606b1d3c4365a0da061c3bf5452f69d7a3a
Roman Shidlovsky <romanshidlovsky@mail.ru>
2022-04-24 23:19:17 +0300

    add displaying order according to the status

--- scripts/loadOrders.js
+++ scripts/loadOrders.js
@@ -17,8 +17,102 @@ function getOrders(){
 
 function Load() {
     const allOrders = JSON.parse(localStorage.getItem("allOrders"));
+
+    let elem = document.getElementById('allOrders');
+    elem.parentNode.removeChild(elem);
+
+    let div = document.getElementById('listAllOrders');
+    let list = document.createElement('ul');
+    list.id = 'allOrders';
+    div.appendChild(list);
+    const draggable_list = document.getElementById('allOrders');
+
+    var colors = ["dodgerblue", "#E85A4F", "limegreen"];
+    var icons = ["fa fa-clock fa-3x", "fa fa-exclamation-circle fa-3x", "fa fa-car-side fa-3x", "fa fa-check fa-3x"];
+    var orderStatusTexts = ["Ожидает начала", "Работник не пришёл", "В процессе", "Заказ завершён"];
+    var StatusColor;
+    var orderStatusPattern = `background: white;
+                            justify-content: center;
+                            align-items: center;
+                            width: 380px;
+                            height: 56px;
+                            border-radius: 20px;`
+
+    function orderStatus(status) {
+        let orderStatusText;
+        let orderStatusIcon;
+        let orderStatusColor;
+        switch (status) {
+            case 0:
+                StatusColor = colors[0];
+                orderStatusColor = `display: flex;
+                            border: 2px solid ${StatusColor};
+                            color: ${StatusColor};` + orderStatusPattern;
+                orderStatusIcon = icons[0];
+                orderStatusText = orderStatusTexts[0];
+                break;
+            case 1:
+                StatusColor = colors[1];
+                orderStatusColor = `display: flex;
+                            border: 2px solid ${StatusColor};
+                            color: ${StatusColor};` + orderStatusPattern;
+                orderStatusIcon = icons[1];
+                orderStatusText = orderStatusTexts[1];
+                break;
+            case 2:
+                StatusColor = colors[1];
+                orderStatusColor = `display: flex;
+                            border: 2px solid ${StatusColor};
+                            color: ${StatusColor};` + orderStatusPattern;
+                orderStatusIcon = icons[1];
+                orderStatusText = orderStatusTexts[1];
+                break;
+            case 3:
+                StatusColor = colors[0];
+                orderStatusColor = `display: flex;
+                            border: 2px solid ${StatusColor};
+                            color: ${StatusColor};` + orderStatusPattern;
+                orderStatusIcon = icons[2];
+                orderStatusText = orderStatusTexts[2];
+                break;
+            case 4:
+                StatusColor = colors[2];
+                orderStatusColor = `display: flex;
+                            border: 2px solid ${StatusColor};
+                            color: ${StatusColor};` + orderStatusPattern;
+                orderStatusIcon = icons[3];
+                orderStatusText = orderStatusTexts[3];
+                break;
+            case 5:
+                StatusColor = colors[2];
+                orderStatusColor = `display: flex;
+                            border: 2px solid ${StatusColor};
+                            color: ${StatusColor};` + orderStatusPattern;
+                orderStatusIcon = icons[3];
+                orderStatusText = orderStatusTexts[3];
+                break;
+        }
+        let orderStatus = `<button style="${orderStatusColor}"
+                                    onmouseover="this.style.backgroundColor='${StatusColor}'; this.style.cursor = 'pointer'; this.style.color = 'white'" onmouseout="this.style.backgroundColor='white'; this.style.color = '${StatusColor}'">
+                                    <i class="${orderStatusIcon}" aria-hidden="true"></i>
+                                    <span class="manager-button-text">${orderStatusText}</span>
+                               </button>`
+        return orderStatus
+    }
+
+    function normalizationOfDate(str) {
+        let year = str.substr(0, 4);
+        let month = str.substr(5, 2);
+        let day = str.substr(8, 2);
+        let hour = str.substr(11, 2);
+        let minutes = str.substr(14, 2);
+        return  day + "/" + month + "/" + year + " " + hour + ":" + minutes
+    }
+
     console.log(allOrders);
-    for(let i = index; i < allOrders.length; i++){
+    allOrders.reverse();
+
+    for(let i = 0; i < allOrders.length; i++) {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
                 <a style="text-decoration: none" href="managerOrderFormation.html">
@@ -28,22 +122,17 @@ function Load() {
                                 <span>${allOrders[i]['orderName']}, ${allOrders[i]['orderLocation']}</span>
                             </div>
                             <div class="order-date">
-                                <span>${allOrders[i]['orderStart']}</span>
+                                <span>${normalizationOfDate(allOrders[i]['orderStart'])}</span>
                             </div>
                         </div>
                         <div class="order-status">
-                            <button class="button manager-button">
-                                <i class="fa fa-exclamation-circle fa-3x" aria-hidden="true"></i>
-                                <span class="manager-button-text">Ожидание начала</span>
-                            </button>
+                            ${orderStatus(allOrders[i]['status'])}
                         </div>
-
                     </div>
                 </a>
                 `;
 
         draggable_list.appendChild(listItem);
-        index = allOrders.length;
     }
 }
 
